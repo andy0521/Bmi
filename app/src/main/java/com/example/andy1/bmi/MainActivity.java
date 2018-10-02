abc874f2f196114f2ca1bca744634f3256b4e20c
andy0521 <andy132888@gmail.com>
2018-10-02 16:33:46 +0800

    view

--- app/src/main/java/com/example/andy1/bmi/MainActivity.java
+++ app/src/main/java/com/example/andy1/bmi/MainActivity.java
@@ -2,12 +2,29 @@ package com.example.andy1.bmi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
 
 public class MainActivity extends AppCompatActivity {
+    public void bmi(View view){
+        Log.d("MainActivity","bmi");
+        EditText edweight = findViewById(R.id.ed_weight);
+        EditText edheight = findViewById(R.id.ed_height);
 
+        String w = edweight.getText().toString();
+        String h = edheight.getText().toString();
+        float weight = Float.parseFloat(w);
+        float height = Float.parseFloat(h);
+        float bmi = weight/(height*height);
+        Log.d("MainActivity","bmi"+" ");
+
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
